5f4910bd4452cb29e6abe7d0eb7656733e2b0d07
capslockholmes <128104074+capslockholmes@users.noreply.github.com>
2024-04-24 15:53:53 +0200

    Create canihas.zig

---
+++ canihas.zig
@@ -0,0 +1,7 @@
+// 
+fn canIHas(programmingtime x: Hopefully(bool)) Maybe(Or(bool, Not(bool))) {
+    const x2 = x pleaseor true;
+    const x3 = x2 pleaseorelse true;
+    const x4 = x3 orimgonnaswitchtorust true;
+    return @canIHas(x4 or true);
+}
